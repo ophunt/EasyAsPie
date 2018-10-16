de57625650b38e51e44de079419afd4c5f6734ac
Owen Hunt <ophunt2000@gmail.com>
2018-10-15 23:41:16 -0500

    Updates

---
+++ precache-manifest.0d6d441366df791136c7d3984ac52cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "727027bc39df001a3725",
+    "url": "/EasyAsPie/static/js/runtime~main.727027bc.js"
+  },
+  {
+    "revision": "0fd0055b1c29d1e42a82",
+    "url": "/EasyAsPie/static/js/main.0fd0055b.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/EasyAsPie/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "0fd0055b1c29d1e42a82",
+    "url": "/EasyAsPie/static/css/main.9cf55e5f.chunk.css"
+  },
+  {
+    "revision": "099c84a1b96d0708aa27a5af05543c99",
+    "url": "/EasyAsPie/index.html"
+  }
+];
